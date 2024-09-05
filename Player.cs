6306fb3e9ab3651f32f0b1151fff9855ec461972
Zjoswaa <joshuakijkuit@hotmail.com>
2024-09-05 20:17:28 +0200

    Continued intro, added Player class.

---
+++ Player.cs
@@ -0,0 +1,17 @@
+﻿public class Player {
+    public string name { get; set; }
+    public int hitPoints { get; set; }
+    public int strength { get; set; }
+    public int agility { get; set;}
+    public int intelligence { get; set;}
+    public int charisma { get; set; }
+    
+    public Player(string name, int strength, int agility, int intelligence, int charisma) {
+        this.hitPoints = 100;
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+        this.charisma = charisma;
+    }
+}
