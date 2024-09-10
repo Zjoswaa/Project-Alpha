24c81dcd75bee306f4ba078082196991a04cafbf
333suki <sogaandmomayez@gmail.com>
2024-09-10 16:24:29 +0200

    Update Monster.cs

--- Monster.cs
+++ Monster.cs
@@ -1,28 +1,4 @@
-﻿//public class Player
-//{
-//    public string Name { get; set; }
-//    public string ClassName { get; set; }
-//    public int HitPoints { get; set; }
-//    public int MaxHitPoints { get; }
-//    public int Strength { get; set; }
-//    public int Agility { get; set; }
-//    public int Intelligence { get; set; }
-//    public int Charisma { get; set; }
-
-//    public Player(string Name, string ClassName, int HitPoints, int Strength, int Agility, int Intelligence, int Charisma)
-//    {
-//        this.Name = Name;
-//        this.ClassName = ClassName;
-//        this.HitPoints = HitPoints;
-//        this.MaxHitPoints = HitPoints;
-//        this.Strength = Strength;
-//        this.Agility = Agility;
-//        this.Intelligence = Intelligence;
-////      this.Charisma = Charisma;
-//    }
-//}
-
-public class Monster
+﻿public class Monster
 {
     public int ID { get; set; }
     public string Name { get; set; }
